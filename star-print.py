8a9203dfe5a4a38ea9021c01f047d10167b3c796
zakir <zakir1997khan@gmail.com>
2023-08-20 09:32:30 +0530

    changes in star file

--- star-print.py
+++ star-print.py
@@ -1,18 +1,19 @@
 
-# left star print
+# left star print with space
 num= int(input('enter your number :'))
 for i in range(num):
     for j in range(i+1):
         print('*', end=" ")
     print()  
 
-#reverse of left star print
+#reverse of left star print with space
 num= int(input('enter your number :'))
 for i in range(num):
     for j in range(num-i):
         print('*', end=" ")
     print()     
 
+# left star print without space
 num = int(input("enter your number :"))
 for i in range(0,num):
     for j in range(0,num-i-1):
